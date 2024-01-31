e1ad96e8ea107c681c9e4a771d84e5b05b22aea2
vincent <vincentcharlebois@gmail.com>
2024-01-30 19:50:50 -0500

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
-import { base } from 'viem/chains';
+import { zora } from 'viem/chains';
 import { getFrameMetadata } from '@coinbase/onchainkit';
 
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
-export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xfac759cefe113e510d495e8ebefa1fa7b1c7b338';
+export const CHAIN = zora;
+export const CONTRACT_ADDRESS = '0x1f1f8f9ab11c6d37170c33d3c04317ef447d47c2';
 export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
