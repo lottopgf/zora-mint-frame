4de0effb1d40738a85c8d021bb5f1c5c2e95d80e
jitzlim <136430551+jitzlim@users.noreply.github.com>
2024-02-01 15:10:47 +0000

    modified:   LICENSE.md 	modified:   public/status/already-minted.png 	modified:   public/status/error.png 	modified:   public/status/no-address.png 	modified:   public/status/out-of-gas.png 	modified:   public/status/recast.png 	modified:   public/status/success.png 	modified:   src/app/opengraph-image.alt.txt 	modified:   src/app/opengraph-image.png 	modified:   src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { getFrameMetadata } from '@coinbase/onchainkit';
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
 
 export const CHAIN = base;
-export const CONTRACT_ADDRESS = '0xfac759cefe113e510d495e8ebefa1fa7b1c7b338';
+export const CONTRACT_ADDRESS = '0xF9a38Deb3a4444689D382514450A1a2cEF5Aa17F';
 export const TOKEN_ID = 1n; // First collection is 1
 
 export const FRAME_METADATA = getFrameMetadata({
